9f9f0157a4370838cb64298be384349a29c7797a
Leo Tran <58055271+phantom037@users.noreply.github.com>
2025-05-05 00:20:57 +1000

    Delete web-template/emily-academy directory

--- web-template/emily-academy/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const mobileMenu = document.querySelector('.mobile-menu');
-    const navLinks = document.querySelector('.nav-links');
-
-    mobileMenu.addEventListener('click', function() {
-        navLinks.classList.toggle('open');
-    });
-});
